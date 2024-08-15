e7a650337f4e45793d6cca341ca5618444d21e8d
Moein Arabi <moein.mo81@gmail.com>
2024-08-15 19:34:53 +0330

    Fix gunicorn worker class to gthread

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -2,4 +2,4 @@
 
 python manage.py migrate
 python manage.py collectstatic --no-input
-gunicorn --bind 0.0.0.0:8000 core.wsgi:application
+gunicorn --bind 0.0.0.0:8000 --worker-class gthread --threads 4 core.wsgi:application
